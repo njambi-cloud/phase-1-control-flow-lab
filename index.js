8cceefc31b055619298eaa318e6818bc47a45f16
Christine <njambs@Christines-MacBook-Pro.local>
2022-11-23 11:31:10 +0300

    work in progress

--- index.js
+++ index.js
@@ -1,11 +1,36 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(someNumber){
   // Write your code here!
+  let x = someNumber
+if (someNumber <= 400) {
+    return ('This one is on me!');  
 }
+else if (someNumber >= 2501) {
+  return ('No can do.');
+}
+else if (someNumber >= 2001) {
+ return ('I will gladly take your thirty bucks.');
+ }
 
-function ternaryCheckCity(){
-  // Write your code here!
 }
+scuberGreetingForFeet(someNumber());
+
+function ternaryCheckCity(someCity){
+  // Write your code here!
+  let y = someCity
+  
+if (someCity === NYC){
+    return ('Ok, sounds good.');
+}
+else {
+return ('No go.');
+
+}
+}
+ternaryCheckCity(someCity());
+
+
 
 function switchOnCharmFromTip(){
   // Write your code here!
 }
+
